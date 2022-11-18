a14c688376aa750d2e7a9a97ee52d6ae516d91f3
meitung473 <a8870506@gmail.com>
2022-11-18 11:29:22 +0800

    deploy

---
+++ assets/index.46f9a394.js
@@ -0,0 +1,1 @@
+import{j as e,M as i,a as t,O as n,b as o,L as r,I as c,S as a}from"./index.d97588d9.js";const d=({navigation:s})=>e(i,{children:[t(n,{context:"image"}),e(o,{children:[s,t(n,{context:"main"}),t(r,{}),e(c,{children:[t(a,{children:"AVG. DISTANCE"}),t(a,{children:"Est. travel time"}),t(n,{context:"subContent"})]})]})]});export{d as default};
---
+++ assets/index.b8dec04d.js
@@ -0,0 +1,1 @@
+import{j as t,M as r,a,O as e,b as s}from"./index.d97588d9.js";const i=({navigation:o})=>t(r,{children:[a(e,{context:"image"}),t(s,{children:[o,a(e,{context:"main"})]})]});export{i as default};
